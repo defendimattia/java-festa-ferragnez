3563f2c2614edc109f552a0b487f0551f85f89e5
defendimattia <defendi.mattia92@gmail.com>
2025-06-24 14:26:54 +0200

    Add cycle for find input guest in array

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -5,17 +5,24 @@ import java.util.Scanner;
 public class CheckGuest {
     public static void main(String[] args) {
 
-        String[] guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+        String[] guests = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+                "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
 
         System.out.println("Come ti chiami?");
         Scanner input = new Scanner(System.in);
         String guestName = input.nextLine();
+        input.close();
 
         // pulizia nome
-        guestName = guestName.toLowerCase().trim().replaceAll("\\s+", " ");
+        guestName = guestName.trim().replaceAll("\\s+", " ");
 
-
-        
-
+        for (int i = 0; i < guests.length; i++) {
+            if (guests[i].equalsIgnoreCase(guestName)) {
+                System.out.println("Sei in lista! Benvenuto alla festa!");
+                break;
+            } else if (i == guests.length - 1 && !guests[i].equalsIgnoreCase(guestName)) {
+                System.out.println("Il tuo nome non è presente nella lista!");
+            }
+        }
     }
 }
