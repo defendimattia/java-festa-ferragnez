b77f9f9bf4dc9d46a01578f92101fb1cf8f0f42c
defendimattia <defendi.mattia92@gmail.com>
2025-06-24 14:09:09 +0200

    Add user input functionality to CheckGuest class

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,10 +1,21 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
 
         String[] guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
 
+        System.out.println("Come ti chiami?");
+        Scanner input = new Scanner(System.in);
+        String guestName = input.nextLine();
+
+        // pulizia nome
+        guestName = guestName.toLowerCase().trim().replaceAll("\\s+", " ");
+
+
         
+
     }
 }
